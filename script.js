f8a4792de8e0781f6bc95566720e137ece7b91a8
Ford Chope <douglas.f.chope@vanderbilt.edu>
2019-05-08 10:43:37 -0500

    if/else

--- script.js
+++ script.js
@@ -1,11 +1,58 @@
 document.addEventListener("DOMContentLoaded", function() { 
   // write your javascript code below.
-  
+  console.log('Test!');
+
+// var age = prompt("how old are you?");
+
+// if (age < 16){
+// 	console.log("YOU CANT DRIVE!!!!!!");
+// 	alert("YOU CANT DRIVE!!!!!!");
+// }	else{
+// 	alert("HERE'S THE KEY TO MY FERRARI!");
+// }
 
 
+// var happyRating = prompt("Rate your happiness on a scale from 1 to 10");
 
+// if(happyRating <= 5){
+// 	alert("Don't worry, you'll get through it");
+// } else{
+// 	alert("That's friggin dope, sick bro");
+// }
 
+// var birthdayIsToday = prompt("is today the big day?");
 
+// if (birthdayIsToday == "yes" ){
+// 	alert("HAPPY BDAY");
+// }	else{
+// 	alert("Good day mate");
+// }
+
+// var taco = prompt("you want steak or chicken?");
+// if(taco == "chicken"){
+// 	alert("thems the best");
+// }	else if(taco == "steak"){
+// 	alert("steak is goat");
+// }	else{
+// 	alert("aint on the menu dog");
+// }
+
+// Thirsty/hungry:
+
+var tummy = prompt("you hangry?");
+var henny = prompt("you thirsty there big guy?");
+
+if (tummy == "yes" && henny == "yes"){
+	alert("cookies and milk");
+}	else if (tummy == "yes" && henny == "no"){
+	alert("just a cookie for you");
+}	else if (tummy == "no" && henny == "yes"){
+	alert("millkkkkk");
+}	else if (tummy == "no" && henny == "no"){
+	alert("get tf outta here");
+}	else{
+	alert("invalid response");
+}
 
 
 
